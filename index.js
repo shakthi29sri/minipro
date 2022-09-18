fe1a0a7bf077b37f5e9fe006748fca2a04ce80a3
shakthi29sri <91943036+shakthi29sri@users.noreply.github.com>
2022-09-18 13:49:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const app = express();
+const app = express();//creates an express application
 app.use(express.static('public'));
-app.set('view engine','ejs');
+app.set('view engine','ejs');//Embedded JavaScript Templating
  
  app.get('/interest',(req,res)=>{
     res.render("interest");
@@ -13,8 +13,9 @@ app.get('/login',(req,res)=>{
     res.render("login");
 });
 app.get('/register',(req,res)=>{
-    res.render("register");
+    res.render("register");// sends the rendered HTML string to the client.
 });
  
 
 app.listen(3101);
+//node index.js
